b9e453a22ae9a22e39e3c52378786375f8b421c3
Arseniy Ivanov <freeatnet@freeatnet.com>
2012-11-17 12:15:00 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixelcardly::Application.config.secret_token = 'd1ca5a98ed0408719e51e06b7c8d51e917f06b347c217c6444ce70b1bdcb2abdc38cf2fee72a3a35a5cbe544307e48c033b7bafc0023ee0876884329ce77c22f'
