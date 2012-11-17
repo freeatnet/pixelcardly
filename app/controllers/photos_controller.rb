013cbd37cb060e615a28bcb899862831cf6b2c8c
Arseniy Ivanov <freeatnet@freeatnet.com>
2012-11-17 17:57:29 -0500

    Adding some better API response format.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -23,17 +23,33 @@ class PhotosController < ApplicationController
       f.json { render status: 400, json: { status: 400, message: "Missing tag parameter."}}
     end and return if params[:tag].blank?
 
+    rpp = 50
+
     params[:distance] = 50 unless params[:distance].present?
     params[:page] = 1 unless params[:page].present? and params[:page].numeric?
 
     latlong_pair = [params[:latitude].to_f, params[:longitude].to_f]
     near_parameter = {"$near" => latlong_pair, "$maxDistance" => params[:distance].to_f}
 
-    photos = Photo.where({location: near_parameter, tags: params[:tag]}).offset((params[:page] - 1) * 50).limit(50)
+    scope_count = Photo.where({location: near_parameter, tags: params[:tag]}).count
+
+    respond_to do |f|
+      f.json { render status: 400, json: {status: 400, message: "Page out of bounds"}}
+    end and return if rpp * params[:page].to_i > scope_count
+
+    offset = (params[:page].to_i - 1) * rpp
+
+    photos = Photo.where({location: near_parameter, tags: params[:tag]}).offset(offset).limit(rpp)
 
     respond_to do |f|
       f.html { render locals: {photos: photos} }
-      f.json { render status: 200, json: photos.to_json }
+      f.json { render status: 200, json: { 
+        total_items: scope_count,
+        total_pages: (scope_count / rpp),
+        current_page: params[:page],
+        photos: photos
+      }
+    }
     end
   end
 
